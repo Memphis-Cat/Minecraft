84fa3f7b37ff9afa1279edc0c8e53879a83164d6
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-07-25 16:29:41 +0100

    Expose asset-backed renderer initialization

--- game/Renderer.h
+++ game/Renderer.h
@@ -1,23 +1,40 @@
 #pragma once
 
 #include "game/World.h"
+
+#include <DirectXMath.h>
 #include <d3d11.h>
 #include <wrl/client.h>
-#include <DirectXMath.h>
+
+#include <filesystem>
 #include <unordered_map>
 
 namespace mc::game {
 class Renderer {
 public:
-    bool Initialize(HWND window, int width, int height);
+    bool Initialize(HWND window,
+                    int width,
+                    int height,
+                    const std::filesystem::path& assetRoot);
     void Resize(int width, int height);
-    void Render(const World& world, DirectX::XMFLOAT3 cameraPosition, DirectX::CXMMATRIX viewProjection, int renderDistanceChunks);
+    void Render(const World& world,
+                DirectX::XMFLOAT3 cameraPosition,
+                DirectX::CXMMATRIX viewProjection,
+                int renderDistanceChunks);
+
 private:
-    struct GpuChunk { Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer; UINT vertexCount{}; std::uint64_t revision{}; };
+    struct GpuChunk {
+        Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
+        UINT vertexCount{};
+        std::uint64_t revision{};
+    };
+
     bool CreateBackBuffer(int width, int height);
     bool CreatePipeline();
-    bool CreateAtlas();
-    bool IsChunkVisible(ChunkCoord coord, DirectX::CXMMATRIX viewProjection) const;
+    bool CreateAtlas(const std::filesystem::path& assetRoot);
+    bool IsChunkVisible(ChunkCoord coord,
+                        DirectX::CXMMATRIX viewProjection) const;
+
     Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain_;
     Microsoft::WRL::ComPtr<ID3D11Device> device_;
     Microsoft::WRL::ComPtr<ID3D11DeviceContext> context_;
@@ -29,9 +46,11 @@ private:
     Microsoft::WRL::ComPtr<ID3D11InputLayout> layout_;
     Microsoft::WRL::ComPtr<ID3D11Buffer> matrixBuffer_;
     Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> atlas_;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> grassColorMap_;
     Microsoft::WRL::ComPtr<ID3D11SamplerState> sampler_;
     Microsoft::WRL::ComPtr<ID3D11RasterizerState> rasterizer_;
     std::unordered_map<ChunkCoord, GpuChunk, ChunkCoordHash> gpuChunks_;
-    int width_{}, height_{};
+    int width_{};
+    int height_{};
 };
 } // namespace mc::game
